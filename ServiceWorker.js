42ab2fc2ea9ad4c48d241bff7dfb132645a0fdd7
kbmhansungb <kbmhansungb@gmail.com>
2024-12-07 21:23:10 +0900

    변경

--- ServiceWorker.js
+++ ServiceWorker.js
@@ -1,4 +1,4 @@
-const cacheName = "FutureVision-GodLiving-0.1";
+const cacheName = "DefaultCompany-GodLiving-0.1.0";
 const contentToCache = [
     "Build/GodLivingPage.loader.js",
     "Build/GodLivingPage.framework.js",
